e61aefe1fb1d0c2faaeda4b37644f4c0aa9df524
Ahyifauziaturohmah <128061646+Ahyifauziaturohmah@users.noreply.github.com>
2023-06-27 12:02:52 +0700

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,24 +1,32 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <vector>
+
 
 using namespace std;
 
-int main()
+int main() 
 {
 	ifstream is("D:\\data.txt");
-	
 	if(!is.is_open())
 	{
-		cout << "file gagal di buka";
+		cout << "tdk dapat membuka file" ;
 		return 1;
-		
 	}
 	
+	vector<string> data;
 	string line;
-	while(getline(is, line))
+	while(getline(is,line))
 	{
-		cout << line << endl;
+		data.push_back(line);
 	}
+	
+	for(auto d:data)
+	{
+		cout << d << endl;
+	}
+	
+	is.close();
 	return 0;
 }
